7fa6617f70a15e520136d2f5cdaf0fd9eedeef8e
Samed Karakus <karakusnavy@gmail.com>
2018-12-12 14:42:29 +0300

    updated

---
+++ MvcAttributeRoting/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcAttributeRoting.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Route("personel/list/{id}/{name}")]
+        public ActionResult PersonelList(int id,string name)
+        {
+            return Content("<center>Welcome To Personel List "+name+". Your Id: "+id+"</center>");
+        }
+        
+        
+    }
+}
---
+++ MvcAttributeRoting/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcAttributeRoting.MvcApplication" Language="C#" %>
